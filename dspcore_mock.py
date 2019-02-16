0dc8b0285263d94b4bc2a0550eeda0e540d93ae7
nyankosoft <sgt.nyanko@gmail.com>
2019-02-16 13:22:59 +0900

    Added definitions of strech/pitchShift APIs. Work in progress.

---
+++ dspcore_mock.py
@@ -0,0 +1,5 @@
+def stretch(src,factor,dest):
+   print('Applying pitch shift (factor: %d)'.format(factor))
+
+def pitchShift(src,shift,dest):
+   print('Applying pitch shift (src: %s, shift: %d, dest: %s)'.format(src,shift,dest))
--- sanic_server.py
+++ sanic_server.py
@@ -16,10 +16,18 @@ import os
 import wget
 
 # Out audio effect library
-import dspcore
+#import dspcore
+
+# Mock of our audio effect library (for testing/debugging)
+import dspcore_mock as dspcore
+
 
 app = Sanic()
+STATIC_FOLDER = os.path.join(os.path.dirname(__file__), 'static')
 url = ""
+
+
+### REST APIs (Public) ###
 
 @app.route('/upload')
 async def upload(request):
@@ -32,14 +40,20 @@ async def upload(request):
 
 @app.route('/pitch_shift')
 async def pitchShift(request):
+    # Applies a pitch shift to the specified file on the cloudinary
+    # Usage:
+    # - http://localhost:8000/pitch_shift?filename=song.wav&shift=50
+    # - The value for shift must be in the range [-100,100]
 
-    factor = request.args['factor'][0]
     filename = request.args['filename'][0]
+    shift = request.args['shift'][0]
 
     # Download the specified file from cloudinary
     download_from_cloundinary(filename)
 
-    dspcore.stretch(factor,filename,output_filename)
+    output_filename = 'pitch_shifted.wav'
+
+    dspcore.pitchShift(filename,shift,output_filename)
 
     # 
     return text('factor: {}, filename: {}'.format(factor,filename))
@@ -73,19 +87,22 @@ async def download_file(request):
     # Download a file from the cloudinary storage
     # Example:
     # - http://localhost:8000/download?filename=song.wav
-    # -> Downloads the file http://res.cloudinary.com/dnh7vvs4n/video/upload/v1550255574/audo_files/song.wav
+    # -> Downloads the file http://res.cloudinary.com/dnh7vvs4n/video/upload/v1550255574/audio_files/song.wav
 
-    cloudinary_folder_url = 'http://res.cloudinary.com/dnh7vvs4n/video/upload/v1550255574/audo_files/'
+    cloudinary_folder_url = 'http://res.cloudinary.com/dnh7vvs4n/video/upload/v1550255574/audio_files/'
 
     filename = request.args['filename'][0]
 
     audio_file_url = cloudinary_folder_url + filename
 
-    logger.info('downloading file: ')
+    logger.info('downloading file: {}'.format(filename))
 
     download_from_cloundinary(audio_file_url)
 
     return text('downloaded file')
+
+
+### Private Functions ###
 
 def download_from_cloundinary(url):
     logger.info('downloading file from c')
@@ -95,12 +112,13 @@ def download_from_cloundinary(url):
     # https://stackoverflow.com/questions/273192/how-can-i-safely-create-a-nested-directory-in-python 
     # for in-depth discussion on this
     dir = 'downloads_from_cloudinary'
-    if not path.exists(dir):
+    if not os.path.exists(dir):
         os.makedirs(dir)
 
-    output_file_path = os.path.join(dir,'output_file.wav')
+    output_file_path = os.path.join(dir,'file.wav')
     logger.info('downloading ' + output_file_path)
-    wget.download(url,output_file_path)
+    f = wget.download(url,output_file_path)
+    logger.info('wget return value: {}'.format(f))
 
 
 ### Test Functions ###
@@ -112,10 +130,35 @@ async def test_upload(request):
 
     return response.json({'url': file_url})
 
+@app.route('/test_download')
+async def test_download(request):
+
+    test_file_url = 'https://res.cloudinary.com/dnh7vvs4n/video/upload/v1550288802/audio_files/download_test_file.wav'
+
+    download_from_cloundinary(test_file_url)
+
+    return text('Downloaded {}'.format(test_file_url))
+
+@app.route('/test_pitch_shift')
+async def test_pitch_shift(request):
+    # Usage
+    # - http://localhost:8000/test_pitch_shift
+
+    shift = 50
+
+    dspcore.pitchShift('testdata/BlueSea_A024.wav',shift,'testdata/pitch_shifted.wav')
+
+    return response.json({'shift': shift})
+
 def init_server():
     logger.info('Initializing the server')
 
+    # the static file serving is only used during debugging
+    app.static('/static', STATIC_FOLDER)
+    app.static('/favicon.ico', os.path.join(STATIC_FOLDER, 'img', 'favicon.ico'))
+
     # Auth thing (Takashi's cloudinary storage)
+    logger.info('Configuring cloudinary')
     ret = cloudinary.config(
     cloud_name = "dnh7vvs4n",
     api_key = "356324651562522",
