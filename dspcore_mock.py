1e74a46850d29061f7f0fb20ea8e6f69d346f541
nikkokun <mizutaninikkou@gmail.com>
2019-02-16 20:39:50 +0900

    fixed errors in dsp core

--- dspcore_mock.py
+++ dspcore_mock.py
@@ -1,21 +1,21 @@
 import os
 
 def stretch(src,factor):
-   print('Applying stretch (factor: {})'.format(factor))
-   return os.path.join('output','transformed.wav')
+	print('Applying stretch (factor: {})'.format(factor))
+	return os.path.join('output','transformed.wav')
 
 def pitchShift(src,shift):
-   print('Applying pitch shift (shift: {})'.format(shift))
-   return os.path.join('output','transformed.wav')
+	print('Applying pitch shift (shift: {})'.format(shift))
+	return os.path.join('output','transformed.wav')
 
 def percussive(src,param):
-   print('Applying percussive: ({})'.format(param))
-   return os.path.join('output','transformed.wav')
+	print('Applying percussive: ({})'.format(param))
+	return os.path.join('output','transformed.wav')
 
 def harmonic(src,param):
-   print('Applying harmonic (param: {})'.format(param))
-   return os.path.join('output','transformed.wav')
+	print('Applying harmonic (param: {})'.format(param))
+	return os.path.join('output','transformed.wav')
 
 def dj(src,param):
-   print('Applying dj (param: {})'.format(param))
-   return os.path.join('output','transformed.wav')
+	print('Applying dj (param: {})'.format(param))
+	return os.path.join('output','transformed.wav')
