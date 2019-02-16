bcec0edf789f44fa25bdbb9b324bd99962ad95f7
Hideo Daikoku <neildaikoku@gmail.com>
2019-02-16 19:52:32 +0900

    Update dspcore.py
    updated functions

--- dspcore.py
+++ dspcore.py
@@ -1,16 +1,47 @@
 import librosa
 import soundfile as sf
+import os
+import numpy as np
 
-filename='testdata/BlueSea_A024.wav'
+def loadFile(filepath):
+   y, sr = librosa.load(filepath)
+   return y,sr
 
-y, sr = librosa.load(filename)
 
-def stretch():
-   y_2 = librosa.effects.time_stretch(y, 0.5)
-   sf.write(filename,y_2,sr)
+def stretch(filename, factor):
+   y,sr= loadFile(filename)
+   y_2 = librosa.effects.time_stretch(y, factor)
+   output_path=os.path.join('./output',os.path.split(filename)[1])
+   sf.write(output_path,y_2,sr)
+   return output_path
 
-def pitchShift():
-   y_2 = librosa.effects.pitch_shift(y, sr, n_steps=4)
-   sf.write(filename,y_2,sr)
+def pitchShift(self.filename, cents):
+   y,sr= loadFile(filename)
+   y_2 = librosa.effects.pitch_shift(y, sr, cents)
+   output_path=os.path.join('./output',os.path.split(filename)[1])
+   sf.write(output_path,y_2,sr)
+   return output_path
 
-pitchShift()
+def percussive(filename, x):
+   y,sr= loadFile(filename)
+   y_2 = librosa.effects.percussive(y, margin=x)
+   output_path=os.path.join('./output',os.path.split(filename)[1])
+   sf.write(output_path,y_2,sr)
+   return output_path
+
+def harmonic(filename, x):
+   y,sr= loadFile(filename)
+   y_2 = librosa.effects.harmonic(y, margin=x)
+   output_path=os.path.join('./output',os.path.split(filename)[1])
+   sf.write(output_path,y_2,sr)
+   return output_path
+
+def dj(filename, size):
+   y,sr= loadFile(filename)
+   _, beat_frames = librosa.beat.beat_track(y=y, sr=sr,hop_length=512)
+   beat_samples = librosa.frames_to_samples(beat_frames)
+   intervals = librosa.util.frame(beat_samples, frame_length=2,hop_length=1).T
+   y_2 = librosa.effects.remix(y, intervals[::size])
+   output_path=os.path.join('./output',os.path.split(filename)[1])
+   sf.write(output_path,y_2,sr)
+   return output_path
